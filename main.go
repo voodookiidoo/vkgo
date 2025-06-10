f87d1aac2745d2c39fbb2e542fea367fd664f645
voodookiidoo <voodookiidoo@yandex.ru>
2025-06-11 00:34:42 +0500

    небольшие исправления и коментарии

--- main.go
+++ main.go
@@ -10,22 +10,26 @@ import (
 )
 
 func main() {
-	pool := NewWorkerPool()
-	pool.StartHandling()
+	pool := NewWorkerPool() //создаём пул
+	pool.StartHandling()    // запускаем обработчик
 	for range 5 {
-		pool.Inc()
+		pool.Inc() // добавляем пять воркеров
 	}
 	for i := range 20 {
 		pool.AddTask(func() any {
-			time.Sleep(time.Millisecond * time.Duration(rand.Intn(500)+200))
+			// запукаем задачи всякие разные
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(300)+300))
 			return fmt.Sprintf("data with id %d and index %d", rand.Int()%300, i)
 		})
 	}
 	pool.Dec()
 	pool.Dec()
-	time.Sleep(time.Second * 4)
+	// удаляем двух воркеров
+	time.Sleep(time.Second * 5)
+	// ждём чтобы таски повыполнялись
 	pool.ShutDown()
-	for data := range pool.outchan {
+	// запускаем завершение пула (всё, из пула теперь только читаем)
+	for data := range pool.dataChan { // и читаем теперь данные из выходного канала
 		fmt.Println("recieved data: ", data)
 	}
 
@@ -33,10 +37,10 @@ func main() {
 
 type WorkerPool struct {
 	taskChan       chan task     // канал по которому таски отправляются горутинам
-	finishChan     chan struct{} // главный канал который завершает работу пула в целом
-	killWorkerChan chan struct{} // канал по которому отправляются запросы за убийство воркера (ужас какой)
-	addWorkerChan  chan struct{} // канал по которому отправляются запросы на добавление таски
-	outchan        chan any      // канал данных по которому их отправляют наружу
+	dataChan       chan any      // канал данных по которому их отправляют наружу
+	shutDownChan   chan struct{} // канал завершения, нужен для завершения (логично)
+	killWorkerChan chan struct{} // канал по которому отправляются запросы на удаление воркера
+	addWorkerChan  chan struct{} // канал по которому отправляются запросы на добавление воркера
 	workerId       atomic.Int32  // айдигенератор для воркера
 }
 
@@ -53,7 +57,7 @@ func (p *WorkerPool) Dec() {
 }
 func (p *WorkerPool) ShutDown() {
 	go func() {
-		p.finishChan <- struct{}{}
+		p.shutDownChan <- struct{}{}
 	}()
 }
 
@@ -68,13 +72,13 @@ func (p *WorkerPool) StartHandling() {
 	go func(ctx context.Context, cancelFunc context.CancelFunc) {
 		for {
 			select {
-			case <-p.finishChan:
+			case <-p.shutDownChan:
 				cancelFunc()
 				wg.Wait()
-				close(p.finishChan)
+				close(p.shutDownChan)
 				close(p.killWorkerChan)
 				close(p.addWorkerChan)
-				close(p.outchan)
+				close(p.dataChan)
 				close(p.taskChan)
 				return
 			case <-p.addWorkerChan:
@@ -97,7 +101,7 @@ func (p *WorkerPool) StartHandling() {
 								return
 							default:
 								fmt.Printf("chan with id %d calculated value %s\n", id, a)
-								p.outchan <- a
+								p.dataChan <- a
 							}
 						}
 
@@ -110,11 +114,11 @@ func (p *WorkerPool) StartHandling() {
 
 func NewWorkerPool() *WorkerPool {
 	return &WorkerPool{
-		finishChan:     make(chan struct{}),
+		shutDownChan:   make(chan struct{}),
 		killWorkerChan: make(chan struct{}),
 		addWorkerChan:  make(chan struct{}),
 		taskChan:       make(chan task),
-		outchan:        make(chan any, 1024),
+		dataChan:       make(chan any, 1024),
 	}
 }
 
